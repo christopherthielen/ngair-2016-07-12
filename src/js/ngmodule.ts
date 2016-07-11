042e38ba9d349a1fb2a25ee23e54d7c5bc7abea4
christopherthielen <github@sandgnat.com>
2016-07-11 18:35:38 -0500

    sq boot with ng-upgrade

--- src/js/ngmodule.ts
+++ src/js/ngmodule.ts
@@ -1,1 +1,1 @@
-export var app = angular.module('ngair', ['ui.router', 'ui.router.visualizer']);
+export var app = angular.module('ngair', ['ui.router', 'ui.router.visualizer', 'ui.router.upgrade']);
